a2ac59d492d5e151729e48a9fb86fb884de2c5ba
inezdoli <iurii.nezdolii@avid.com>
2022-10-27 17:25:36 +0300

    first commit ex 7 v 0

--- src/main/java/Lection_JIP_part2/Exercise_7.java
+++ src/main/java/Lection_JIP_part2/Exercise_7.java
@@ -1,7 +1,42 @@
 package Lection_JIP_part2;
 
-public class Exercise_7 {
-    public static void main(String[] args) {
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
 
+/*
+Write a program asking the user for five integers (Don't use arrays!). The program
+then outputs the sum of squares.
+a) Learn how you can start a Java program from the Windows/macOS console, i.e.
+without the IDE (i.e. without IntelliJ IDEA)! – Document clearly how it works! –
+Your knowledge about using the console should help you.
+b) A menu should be implemented with following options: [input numbers],
+[calculate sum of squares] and [quit]. All options (except [quit]) return to the
+menu after their action has taken place. Erroneous user inputs (e.g. text instead
+of an integer) should be checked by the program and require new input from the
+user.
+ */
+public class Exercise_7 {
+   int counter;
+    List<Integer> list=new Vector<>();
+    public static void main(String[] args) {
+        int sum;
+        int number=0;
+        Scanner scan=new Scanner(System.in);
+        for(int i=0;i<count;i++){
+            System.out.println("Введіть будь ласка число - ");
+            list.add(scan.nextInt());
+
+        }
+        System.out.println("Sum all number ="+ sum);
+
+    }
+
+    int calculateSum(){
+        int result=0;
+        for (:
+             ) {
+
+        }
     }
 }
