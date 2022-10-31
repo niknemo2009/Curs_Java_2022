67121043bb4f3b1ddcb76a3ab410005e85a18da2
inezdoli <iurii.nezdolii@avid.com>
2022-10-31 09:18:20 +0200

    next commit refactoring and new task

--- src/main/java/lection5/Exercise11.java
+++ src/main/java/lection5/Exercise11.java
@@ -2,6 +2,9 @@ package lection5;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /*
@@ -10,7 +13,11 @@ user shall enter one word after another and must be able to repeat the chain of 
 The program should have a menu to control the flow of the game. (You'll need a way to
 clear the console in Java, please see ....
  */
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!! No COMPLETE
 public class Exercise11 {
+    List<String> data=new ArrayList<>();
+    List<String> nameUsers=new ArrayList<>();
+    Scanner scan=new Scanner(System.in);
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(new File("/home/yurii/IdeaProjects/Curs_Java_2022/src/main/resources/1.txt"));
@@ -18,7 +25,28 @@ public class Exercise11 {
         int var1 = scan.nextInt();
 
     }
+void viewMenu(){
+        boolean flag=true;
+    System.out.println("1. Start game.");
+    System.out.println("2. Quit.");
 
+        while (flag){
+            switch ()
+
+        }
+
+}
+void nextStep(){
+  String nextStr=scan.nextLine();
+   if(data.size()>0){
+       String[] previousString=data.get(data.size()-1).split(" ");
+       String[] lastString=nextStr.split(" ");
+       if(previousString.length!=lastString.length)
+       Arrays.sort(previousString);
+       Arrays.sort(lastString);
+
+   }
+}
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
